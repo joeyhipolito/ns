592382f252894c2aa068a9fd28c9aad482a31743
Joey Hipolito <hi@joeyhipolito.com>
2014-10-07 03:44:59 +0800

    register and login auth complete

--- app/auth/routes.js
+++ app/auth/routes.js
@@ -1,7 +1,8 @@
 var User = require('app/user/UserModel');
 
 function login (req, res) {
-}
+  res.json(req.user);
+};
 
 function register (req, res) {
   res.json({message: 'successfully registered'});
@@ -11,8 +12,14 @@ function session (req, res) {
   res.json({ some: true, another: 'cool'});
 };
 
+function logout (req, res) {
+  req.logout();
+};
+
 function setup (router, passport) {
   router.get('/auth', session);
+  router.post('/auth', passport.authenticate('login'), login);
+  router.delete('/auth', logout);
   router.post('/register', passport.authenticate('register'), register);
 };
 
